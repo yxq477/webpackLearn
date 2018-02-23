cd9edbb585d6e1a0e0c7ce5f8983be6dc87ea415
boblunt <yxq477@gmail.com>
2018-02-23 23:04:25 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import _ from 'lodash'
-
+import './style.css'
 function componnet () {
   var element = document.createElement('div')
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.classList.add('hello')
   return element
 }
 
