a6fdf50820ccf999b9e31cfabad56e33dd809a0b
boblunt <yxq477@gmail.com>
2018-02-25 00:41:18 +0800

    开发

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,14 @@ module.exports = {
     app: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
